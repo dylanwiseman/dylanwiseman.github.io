35d0909e2e050b02bfc76756c2630734dd62dab5
Dylan Wiseman <dylan.wiseman22@gmail.com>
2021-07-29 12:01:27 -0600

    formatting

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
-const img = document.querySelector('img')
-const h1 = document.querySelector('h1')
-const a = document.querySelectorAll('a')
-const h6 = document.querySelectorAll('h6')
-let toggle = false
+const img = document.querySelector("img");
+const h1 = document.querySelector("h1");
+const a = document.querySelectorAll("a");
+const h6 = document.querySelectorAll("h6");
+let toggle = false;
 
 const redTheme = () => {
-    if (toggle === false) {
-    img.src="8-bit-dylan-red.png"
-    h1.textContent="Hi, I'm Dylan :("
-    toggle = true
-    } else {
-    img.src="8-bit-dylan.png"
-    h1.textContent="Hi, I'm Dylan :)"
-    toggle = false
-    }
-    document.body.classList.toggle('red')
-    img.classList.toggle('red-img')
-    for (let i in a) {
-        a[i].classList.toggle('red-link')
-    }
-    for (let i in h6) {
-        h6[i].classList.toggle('red-border')
-    }
+  if (toggle === false) {
+    img.src = "8-bit-dylan-red.png";
+    h1.textContent = "Hi, I'm Dylan :(";
+    toggle = true;
+  } else {
+    img.src = "8-bit-dylan.png";
+    h1.textContent = "Hi, I'm Dylan :)";
+    toggle = false;
+  }
+  document.body.classList.toggle("red");
+  img.classList.toggle("red-img");
+  for (let i in a) {
+    a[i].classList.toggle("red-link");
+  }
+  for (let i in h6) {
+    h6[i].classList.toggle("red-border");
+  }
+};
 
-}
-
-img.addEventListener('click',redTheme)
+img.addEventListener("click", redTheme);
