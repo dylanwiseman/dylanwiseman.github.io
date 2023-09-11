f83973babad2a5357b5450166e02a199e4488794
Dylan Wiseman <dylan.wiseman22@gmail.com>
2023-09-11 16:07:32 -0600

    nextui setup

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import aloe from "./images/aloe.png";
 import Project from "./Components/Project";
 import { projects } from "./projects";
 import { professional } from "./professional";
+import { NextUIProvider } from "@nextui-org/react";
 
 function App() {
   const projectsArray = projects?.map((project, index) => {
@@ -40,64 +41,68 @@ function App() {
   });
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className="about-div">
-          <img src={bitDylan} alt="8-bit Dylan" className="profpic" />
-          <h1>Dylan Wiseman</h1>
-          <h3>Full-Stack Software Engineer</h3>
-          <h5>also sci-fi author, Spotify artist, and occasional party host</h5>
-          <div id="contact-div">
-            <a
-              className="cta"
-              href="mailto:dylan.wiseman22@gmail.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Send me an email
-            </a>
-            <a
-              href="https://github.com/dylanwiseman"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              GitHub
-            </a>
-            <p className="divider">|</p>
-            <a
-              href="https://www.linkedin.com/in/jdylanwiseman/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              LinkedIn
-            </a>
-            <p className="divider">|</p>
-            <a
-              href="https://docs.google.com/document/d/1uKF8PeMs7TatjTy9rwlLAElnaZjqvix90ZGTzyKGiuw/edit"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Resume
-            </a>
+    <NextUIProvider>
+      <div className="App">
+        <header className="App-header">
+          <div className="about-div">
+            <img src={bitDylan} alt="8-bit Dylan" className="profpic" />
+            <h1>Dylan Wiseman</h1>
+            <h3>Full-Stack Software Engineer</h3>
+            <h5>
+              also sci-fi author, Spotify artist, and occasional party host
+            </h5>
+            <div id="contact-div">
+              <a
+                className="cta"
+                href="mailto:dylan.wiseman22@gmail.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Send me an email
+              </a>
+              <a
+                href="https://github.com/dylanwiseman"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                GitHub
+              </a>
+              <p className="divider">|</p>
+              <a
+                href="https://www.linkedin.com/in/jdylanwiseman/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                LinkedIn
+              </a>
+              <p className="divider">|</p>
+              <a
+                href="https://docs.google.com/document/d/1uKF8PeMs7TatjTy9rwlLAElnaZjqvix90ZGTzyKGiuw/edit"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Resume
+              </a>
+            </div>
           </div>
-        </div>
-        <div className="experience-div">
-          <h2>Professional:</h2>
-          <div className="project-container">
-            <img src={plant2} alt="plant 2" className="plant plant2" />
-            <img src={hibiscus} alt="plant 1" className="plant hibiscus" />
-            {professionalArray}
+          <div className="experience-div">
+            <h2>Professional:</h2>
+            <div className="project-container">
+              <img src={plant2} alt="plant 2" className="plant plant2" />
+              <img src={hibiscus} alt="plant 1" className="plant hibiscus" />
+              {professionalArray}
+            </div>
           </div>
-        </div>
-        <div id="projects-div">
-          <h2>Personal:</h2>
-          <div className="project-container">
-            <img src={aloe} alt="plant 1" className="plant aloe" />
-            {projectsArray}
+          <div id="projects-div">
+            <h2>Personal:</h2>
+            <div className="project-container">
+              <img src={aloe} alt="plant 1" className="plant aloe" />
+              {projectsArray}
+            </div>
           </div>
-        </div>
-      </header>
-    </div>
+        </header>
+      </div>
+    </NextUIProvider>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+
+//@ts-ignore
+const { nextui } = require("@nextui-org/react");
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 };
