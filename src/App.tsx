3b6ac714cc0d5732e12312dffb557070b9507bf5
Dylan Wiseman <dylan.wiseman22@gmail.com>
2023-09-15 16:05:35 -0600

    about me

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import "./App.css";
-import bitDylan from "./images/bitDylan.png";
 import plant2 from "./images/houseplant2.png";
+import mail from "./images/mail.png";
 import hibiscus from "./images/hibiscus.png";
 import aloe from "./images/aloe.png";
-import Project from "./Components/Project";
 import ProjectCard from "./Components/ProjectCard";
 import { projects } from "./projects";
 import { professional } from "./professional";
 import { NextUIProvider } from "@nextui-org/react";
-// import Header from "./Components/Header";
+import { Image, Button } from "@nextui-org/react";
 import GlassCard from "./Components/HeaderCard";
 
 function App() {
@@ -67,9 +66,42 @@ function App() {
           </div>
         </div>
         <div className="flex flex-col lg:flex-row justify-center">
-          <div className="experience-div lg:mr-8">
+          <div className="experience-div lg:max-w-lg xl: max-w-xl lg:mr-8">
             <h2 className="mt-4 text-xl">Professional:</h2>
             <div className="project-container">{professionalArray}</div>
+            <div>
+              <h2>How did we get here?</h2>
+              <p>
+                I graduated BYU in 2019 with a degree in Marketing. After two
+                years of tech sales, I decided I wanted to build the products
+                instead and learned to code at Devmountain in 2021. On top of my
+                dev work, I'm a sci-fi author at Rees Literary Agency, an artist
+                on Spotify, and a photographer in my free time. If you want to
+                offer me a job, collab on a song, or shoot some pics, send me an
+                email.
+              </p>
+              <Button
+                isIconOnly
+                className="data-[hover]:bg-foreground/0 justify-start mt-2 w-full justify-center"
+                radius="full"
+                variant="light"
+              >
+                <a href="mailto:dylan.wiseman22@gmail.com">
+                  <Image
+                    alt="Album cover"
+                    className="object-cover"
+                    height={25}
+                    width={25}
+                    shadow="none"
+                    src={mail}
+                  />
+                </a>
+              </Button>
+              <p>
+                FYI this portfolio is built on React using Typescript,
+                Javascript, TailwindCSS, and NextUI.
+              </p>
+            </div>
           </div>
           <div id="projects-div">
             <h2 className="mt-4 text-xl">Personal:</h2>
