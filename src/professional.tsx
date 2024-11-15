28200fdcbf5326786f2b8e1b776a0ebc696f893e
Dylan Wiseman <85514881+dylanwiseman@users.noreply.github.com>
2024-11-14 21:05:04 -0800

    Update professional.tsx

--- src/professional.tsx
+++ src/professional.tsx
@@ -4,7 +4,7 @@ import techshed from "./images/tshed.jpg";
 export const professional = [
   {
     name: "Weave",
-    dates: "Dec 2021 - Feb 2023",
+    dates: "Dec 2021 - Jun 2023",
     title: "Frontend Developer",
     link: "https://www.getweave.com",
     image: weave,
@@ -25,28 +25,28 @@ export const professional = [
       },
     ],
   },
-  {
-    name: "Techshed",
-    dates: "Jun 2023 - Present",
-    title: "Fullstack Engineer",
-    link: "https://thetechshed.org/",
-    image: techshed,
-    github: "https://github.com/hanfamilym1/tech-shed",
-    desc: "Building out Techshed's website and mananging APIs, CMS connections, and blog content.",
-    tags: ["React", "Typescript", "Next.js", "GraphQL", "TailwindCSS"],
-    // reviews: [
-    //   {
-    //     body: "Dylan seems to pick things up immediately, no matter the difficulty or learning curve.",
-    //     title: "Spencer Dixon, Sr UX/UI Designer",
-    //   },
-    //   {
-    //     body: "Dylan is extremely talented and well rounded and will be an asset to any team.",
-    //     title: "Brett Peterson, Director of Marketing Dev",
-    //   },
-    //   {
-    //     body: "10/10, would work with again.",
-    //     title: "Mike Morris, Sr Web Developer",
-    //   },
-    // ],
-  },
+  // {
+  //   name: "Techshed",
+  //   dates: "Jun 2023 - Present",
+  //   title: "Fullstack Engineer",
+  //   link: "https://thetechshed.org/",
+  //   image: techshed,
+  //   github: "https://github.com/hanfamilym1/tech-shed",
+  //   desc: "Building out Techshed's website and mananging APIs, CMS connections, and blog content.",
+  //   tags: ["React", "Typescript", "Next.js", "GraphQL", "TailwindCSS"],
+  //   // reviews: [
+  //   //   {
+  //   //     body: "Dylan seems to pick things up immediately, no matter the difficulty or learning curve.",
+  //   //     title: "Spencer Dixon, Sr UX/UI Designer",
+  //   //   },
+  //   //   {
+  //   //     body: "Dylan is extremely talented and well rounded and will be an asset to any team.",
+  //   //     title: "Brett Peterson, Director of Marketing Dev",
+  //   //   },
+  //   //   {
+  //   //     body: "10/10, would work with again.",
+  //   //     title: "Mike Morris, Sr Web Developer",
+  //   //   },
+  //   // ],
+  // },
 ];
